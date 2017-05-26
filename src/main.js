c53746b7f46019a209ad0ac967dc9d5d5fe832b2
Ellen Duong <ellen@vagvlan536.1082.wlan.wireless-pennnet.upenn.edu>
2017-05-26 15:52:45 -0400

    Resized image, prepped for crumpling

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Framework from './framework'
 import Noise from './noise'
 import {other} from './noise'
 
+
+var backMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.BackSide});
+
 // called after the scene loads
 function onLoad(framework) {
   var scene = framework.scene;
@@ -29,9 +32,8 @@ function onLoad(framework) {
   // var {scene, camera, renderer, gui, stats} = framework; 
 
   // initialize a simple box and material
-  
-  var p = drawPhoto('https://raw.githubusercontent.com/eldu/CrumpledPaper/master/adam.jpg');
-  scene.add(p);
+  var p = drawPhoto('https://raw.githubusercontent.com/eldu/CrumpledPaper/master/adam.jpg', scene);
+  // scene.add(p);
 
   // set camera position
   camera.position.set(1, 1, 2);
@@ -45,26 +47,38 @@ function onLoad(framework) {
 }
 
 // Returns 
-function drawPhoto(url) {
-  // initialize a simple box and material
-  var plane = new THREE.PlaneGeometry(1, 1, 20, 20);
+function drawPhoto(url, scene) {
+  var texture = new THREE.TextureLoader().load(url, function(tex) {
+    // Callback Image is loaded
 
-  var frontMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      image: { // Check the Three.JS documentation for the different allowed types and values
-        type: "t", 
-        value: THREE.ImageUtils.loadTexture(url)
-      }
-    },
-    side: THREE.FrontSide,
-    wireframe: true,
-    vertexShader: require('./shaders/photo-vert.glsl'),
-    fragmentShader: require('./shaders/photo-frag.glsl')
+    // Define plane
+    var w = texture.image.width / texture.image.height;
+    var h = 1;
+    var plane = new THREE.PlaneGeometry(w, h, 20, 20);
+
+    // Create photo material
+    var frontMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        image: { // Check the Three.JS documentation for the different allowed types and values
+          type: "t", 
+          value: texture
+        }
+      },
+      side: THREE.FrontSide,
+      wireframe: true,
+      vertexShader: require('./shaders/photo-vert.glsl'),
+      fragmentShader: require('./shaders/photo-frag.glsl')
+    });
+
+
+    // TODO: Crumple
+
+    // Create Mesh
+    var photo = THREE.SceneUtils.createMultiMaterialObject(plane, [frontMaterial, backMaterial]);
+    
+    // Add to scene
+    scene.add(photo);
   });
-  var backMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.BackSide});
-
-  var photo = THREE.SceneUtils.createMultiMaterialObject(plane, [frontMaterial, backMaterial]);
-  return photo;
 }
 
 function randomRange(min, max) { //  Generates a random number in range [min, max]
