99fa3db2b859a0a9812902e27e6507b793b7fc3a
Ellen Duong <ellen@vagvlan536.1082.wlan.wireless-pennnet.upenn.edu>
2017-05-26 14:41:01 -0400

    reformmated code

--- src/main.js
+++ src/main.js
@@ -29,35 +29,50 @@ function onLoad(framework) {
   // var {scene, camera, renderer, gui, stats} = framework; 
 
   // initialize a simple box and material
-  var plane = new THREE.PlaneGeometry(1, 1);
-
-  var adamMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      image: { // Check the Three.JS documentation for the different allowed types and values
-        type: "t", 
-        value: THREE.ImageUtils.loadTexture('./adam.jpg')
-      }
-    },
-    side: THREE.FrontSide,
-    vertexShader: require('./shaders/adam-vert.glsl'),
-    fragmentShader: require('./shaders/adam-frag.glsl')
-  });
-  var backMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.BackSide});
-  var materials = [adamMaterial, backMaterial];
-
-  var adamCube = THREE.SceneUtils.createMultiMaterialObject(plane, materials);
+  
+  var p = drawPhoto('https://raw.githubusercontent.com/eldu/CrumpledPaper/master/adam.jpg');
+  scene.add(p);
 
   // set camera position
   camera.position.set(1, 1, 2);
   camera.lookAt(new THREE.Vector3(0,0,0));
-
-  scene.add(adamCube);
 
   // edit params and listen to changes like this
   // more information here: https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
   gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
     camera.updateProjectionMatrix();
   });
+}
+
+// Returns 
+function drawPhoto(url) {
+  // initialize a simple box and material
+  var plane = new THREE.PlaneGeometry(1, 1, 20, 20);
+
+  var frontMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      image: { // Check the Three.JS documentation for the different allowed types and values
+        type: "t", 
+        value: THREE.ImageUtils.loadTexture(url)
+      }
+    },
+    side: THREE.FrontSide,
+    wireframe: true,
+    vertexShader: require('./shaders/photo-vert.glsl'),
+    fragmentShader: require('./shaders/photo-frag.glsl')
+  });
+  var backMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.BackSide});
+
+  var photo = THREE.SceneUtils.createMultiMaterialObject(plane, [frontMaterial, backMaterial]);
+  return photo;
+}
+
+function randomRange(min, max) { //  Generates a random number in range [min, max]
+  return Math.random() * (max - min) + min;
+}
+
+function randomMax(max) { // Generates random number in range [0, max]
+  return Math.random() * max;
 }
 
 // called on frame updates
--- src/shaders/photo-frag.glsl
+++ src/shaders/photo-frag.glsl
@@ -5,7 +5,5 @@ uniform sampler2D image;
 
 void main() {
   vec4 color = texture2D( image, vUv );
-
   gl_FragColor = vec4( color.rgb, 1.0 );
-
 }
